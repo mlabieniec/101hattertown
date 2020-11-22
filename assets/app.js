1f9ee37c3557632a010a561114bd052fec7c0164
Michael Labieniec <michael@pop-os.localdomain>
2020-11-22 09:37:11 -0500

    text updates and styles

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 (function() {
 
-    document.querySelector("#gallery").addEventListener("click", function(event) {
+    document.querySelector("#galleries").addEventListener("click", function(event) {
         if (event.target.matches("img")) {
             window.open(event.target.src, "_blank");
         }
